9e39a466cea3d6e325b800982365485431c718cf
braydentodd <braydentodd12@gmail.com>
2023-11-16 14:46:33 -0500

    remove backend

--- app.js
+++ app.js
@@ -1,74 +1,40 @@
-document.addEventListener('DOMContentLoaded', async () => {
+document.addEventListener('DOMContentLoaded', () => {
     console.log('DOMContentLoaded event fired!');
-    const appElement = document.getElementById('app');
   
     // Load Bible text from the JSON file
-    const bibleTextResponse = await fetch('./bibleText.json');
-    const bibleText = await bibleTextResponse.json();
+    fetch('./bibleText.json')
+        .then(response => response.json())
+        .then(bibleText => {
+            // Fetch the Bible chapter content for 1st Timothy chapter 1
+            const chapter1Text = bibleText["1Timothy"]["chapter1"];
   
-    // Fetch the Bible chapter content for 1st Timothy chapter 1
-    const chapter1Text = bibleText["1Timothy"]["chapter1"];
+            // Render the Bible chapter in the middle column
+            const middleColumn = document.getElementById('middle-column');
+            middleColumn.innerHTML = chapter1Text.join('<br>');
   
-    // Render the Bible chapter in the middle column
-    const middleColumn = document.getElementById('middle-column');
-    middleColumn.innerHTML = chapter1Text.join('<br>');
+            // Include Disqus script (replace 'your-disqus-shortname' with your Disqus shortname)
+            const disqusScript = document.createElement('script');
+            disqusScript.src = 'https://your-disqus-shortname.disqus.com/embed.js';
+            disqusScript.setAttribute('data-timestamp', +new Date());
+            document.head.appendChild(disqusScript);
   
-    // Create the left column for comments
-    const leftColumn = document.getElementById('left-column');
+            // Display Disqus comments section
+            const disqusContainer = document.createElement('div');
+            disqusContainer.id = 'disqus_thread';
+            middleColumn.appendChild(disqusContainer);
   
-    // Create the right column for viewing comments
-    const rightColumn = document.getElementById('right-column');
+            // Event listener for highlighting text and adding comments
+            const handleSelection = () => {
+                const selection = window.getSelection();
+                const selectedText = selection.toString().trim();
   
-    // Function to save comments
-    const saveComment = async (verse, comment) => {
-        const response = await fetch('https://choice-project-0d60278c0263.herokuapp.com/api/comments', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ verse, comment }),
-        });
-        const newComment = await response.json();
-        renderComments(verse);
-        return newComment;
-    };
-  
-    // Function to render comments for a specific verse
-    const renderComments = async (verse) => {
-        const response = await fetch(`https://choice-project-0d60278c0263.herokuapp.com/api/comments/${verse}`);
-        const comments = await response.json();
-  
-        const commentsHtml = comments.map(comment => `<p>${comment.comment}</p>`).join('');
-        rightColumn.innerHTML = `<h2>Comments/Reflections for ${verse}</h2>${commentsHtml}`;
-    };
-  
-    // Event listener for highlighting text and adding comments
-    const handleSelection = () => {
-        const selection = window.getSelection();
-        const selectedText = selection.toString().trim();
-  
-        if (selectedText !== '') {
-            console.log('Selected Text:', selectedText);
-  
-            const commentInput = document.createElement('textarea');
-            commentInput.id = 'comment-input';
-            leftColumn.innerHTML = '<h2>Add Comment/Reflection</h2>';
-            leftColumn.appendChild(commentInput);
-  
-            const commentButton = document.createElement('button');
-            commentButton.id = 'comment-button';
-            commentButton.textContent = 'Save Comment';
-            commentButton.addEventListener('click', async () => {
-                const comment = commentInput.value.trim();
-                if (comment !== '') {
-                    await saveComment(selectedText, comment);
+                if (selectedText !== '') {
+                    console.log('Selected Text:', selectedText);
                 }
-            });
-            leftColumn.appendChild(commentButton);
-        }
-    };
+            };
   
-    middleColumn.addEventListener('mouseup', handleSelection);
-    middleColumn.addEventListener('touchend', handleSelection);
-  });
-  
+            middleColumn.addEventListener('mouseup', handleSelection);
+            middleColumn.addEventListener('touchend', handleSelection);
+        })
+        .catch(error => console.error('Error fetching Bible text:', error));
+});
