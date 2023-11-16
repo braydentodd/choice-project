5f158541c4325e008a4ecc9a1466f4f46a51bafa
braydentodd <braydentodd12@gmail.com>
2023-11-16 10:08:26 -0500

    structuring verses

--- frontend/app.js
+++ frontend/app.js
@@ -1,71 +1,77 @@
 document.addEventListener('DOMContentLoaded', async () => {
-    const appElement = document.getElementById('app');
-  
-    // Fetch the Bible chapter content
-    const chapterResponse = await fetch('https://git.heroku.com/choice-project.git/api/esv/1+Timothy+1');
-    const chapterHtml = await chapterResponse.text();
-  
-    // Render the Bible chapter in the middle column
-    const middleColumn = document.createElement('div');
-    middleColumn.id = 'middle-column';
-    middleColumn.innerHTML = chapterHtml;
-    appElement.appendChild(middleColumn);
-  
-    // Create the left column for comments
-    const leftColumn = document.createElement('div');
-    leftColumn.id = 'left-column';
-    appElement.appendChild(leftColumn);
-  
-    // Create the right column for viewing comments
-    const rightColumn = document.createElement('div');
-    rightColumn.id = 'right-column';
-    appElement.appendChild(rightColumn);
-  
-    // Function to save comments
-    const saveComment = async (verse, comment) => {
+  const appElement = document.getElementById('app');
+
+  // Load Bible text from the JSON file
+  const bibleTextResponse = await fetch('./bibleText.json');
+  const bibleText = await bibleTextResponse.json();
+
+  // Fetch the Bible chapter content for 1st Timothy chapter 1
+  const chapter1Text = bibleText["1Timothy"]["chapter1"];
+
+  const versesElement = document.getElementById('verses');
+  versesElement.innerHTML = chapter1Text.join('<br>');
+
+  // Render the Bible chapter in the middle column
+  const middleColumn = document.getElementById('middle-column');
+  middleColumn.innerHTML = chapter1Text.join('<br>');
+
+  // Create the left column for comments
+  const leftColumn = document.getElementById('left-column');
+
+  // Create the right column for viewing comments
+  const rightColumn = document.getElementById('right-column');
+
+  // Function to save comments
+  const saveComment = async (verse, comment) => {
+      // Mock API endpoint for saving comments (replace with your actual API endpoint)
       const response = await fetch('/api/comments', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ verse, comment }),
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ verse, comment }),
       });
       const newComment = await response.json();
       renderComments(verse);
       return newComment;
-    };
-  
-    // Function to render comments for a specific verse
-    const renderComments = async (verse) => {
+  };
+
+  // Function to render comments for a specific verse
+  const renderComments = async (verse) => {
+      // Mock API endpoint for fetching comments (replace with your actual API endpoint)
       const response = await fetch(`/api/comments/${verse}`);
       const comments = await response.json();
-  
+
       const commentsHtml = comments.map(comment => `<p>${comment.comment}</p>`).join('');
       rightColumn.innerHTML = `<h2>Comments/Reflections for ${verse}</h2>${commentsHtml}`;
-    };
-  
-    // Event listener for highlighting text and adding comments
-    middleColumn.addEventListener('mouseup', (event) => {
+  };
+
+  // Event listener for highlighting text and adding comments
+  const handleSelection = () => {
       const selection = window.getSelection();
       const selectedText = selection.toString().trim();
-  
+
       if (selectedText !== '') {
-        const commentInput = document.createElement('textarea');
-        commentInput.id = 'comment-input';
-        leftColumn.innerHTML = '<h2>Add Comment/Reflection</h2>';
-        leftColumn.appendChild(commentInput);
-  
-        const commentButton = document.createElement('button');
-        commentButton.id = 'comment-button';
-        commentButton.textContent = 'Save Comment';
-        commentButton.addEventListener('click', async () => {
-          const comment = commentInput.value.trim();
-          if (comment !== '') {
-            await saveComment(selectedText, comment);
-          }
-        });
-        leftColumn.appendChild(commentButton);
+          console.log('Selected Text:', selectedText);
+
+          const commentInput = document.createElement('textarea');
+          commentInput.id = 'comment-input';
+          leftColumn.innerHTML = '<h2>Add Comment/Reflection</h2>';
+          leftColumn.appendChild(commentInput);
+
+          const commentButton = document.createElement('button');
+          commentButton.id = 'comment-button';
+          commentButton.textContent = 'Save Comment';
+          commentButton.addEventListener('click', async () => {
+              const comment = commentInput.value.trim();
+              if (comment !== '') {
+                  await saveComment(selectedText, comment);
+              }
+          });
+          leftColumn.appendChild(commentButton);
       }
-    });
-  });
-  
+  };
+
+  middleColumn.addEventListener('mouseup', handleSelection);
+  middleColumn.addEventListener('touchend', handleSelection);
+});
